067fd728f3b6255197431c6a570df95a6a314fa4
joaquin-munoz <joaquin.munoz.c01@mail.pucv.cl>
2025-10-29 17:21:33 -0300

    interfaz colisionable.java
    interfaz añadida

---
+++ colisionable.java
@@ -0,0 +1,11 @@
+package puppy.code;
+
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Interfaz para objetos que pueden colisionar en el juego.
+ */
+public interface Colisionable {
+    Rectangle getArea();
+    void onColision(Colisionable otro);
+}
